9ad27fce35f2235cc8028101f9d7c3ca9cfbb036
Daniel Foth <sorax2204@hotmail.com>
2020-03-07 16:57:53 +0000

    Chose data set and started regression

--- code/Regression/linear_regression.py
+++ code/Regression/linear_regression.py
@@ -1,43 +1,36 @@
 import numpy as np
 import pandas as pd
-import scipy.stats as stats
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.datasets import load_boston
 
-boston = load_boston()
+data = pd.read_csv("../dataset/adult.data")
+data[data.isnull().any(axis=1)]
 
-print (boston.keys())
-print (boston.data.shape)
+print(data.head())
+print(data.keys())
+print(data.shape)
+print(data.describe())
 
-print(boston.feature_names)
+lb_make = LabelEncoder()
+ohe = OneHotEncoder(sparse=False)
+data['salary'] = lb_make.fit_transform(data['salary'])
 
-print (boston.DESCR)
+transformed = ohe.fit_transform(data[['occupation']])
+ohe_df = pd.DataFrame(transformed, columns=[w.replace('x0', 'occupation') for w in ohe.get_feature_names()])
+data = pd.concat([data, ohe_df], axis=1).drop(['occupation'], axis=1)
 
+x = data[['age']]
+x = data.drop(['salary', 'workclass', 'fnlwgt', 'education', 'marital-status', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss','native-country'], axis = 1)
+y = data[['salary']]
 
-bos = pd.DataFrame(boston.data)
-
-print(bos.head())
-
-bos.columns = boston.feature_names
-print(bos.head())
-
-
-print (boston.target.shape)
-
-
-bos['PRICE'] = boston.target
-
-print (bos.head())
-
-print(bos.describe())
-
-
-x= bos.drop('PRICE', axis=1)
-y = bos['PRICE']
+print(x.head())
+print(x.keys())
+print(x.shape)
+print(x.describe())
 
 X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size = 0.33, random_state = 8)
 
@@ -52,12 +45,6 @@ model = LinearRegression()
 model.fit(X_train, Y_train)
 
 Y_Pred = model.predict(X_test)
-
-
-plt.scatter(Y_test, Y_Pred)
-
-plt.show()
-
 
 rmse = np.sqrt(mean_squared_error(Y_test,Y_Pred))
 
